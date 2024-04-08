83acc0162bdb296179237b6f24569f4abe72b990
Wambui <nderituwambui93@gmail.com>
2024-04-09 00:46:45 +0300

    flow lab

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceInFeet) {
+  if (distanceInFeet <= 400) {
+    return 'This one is on me!';
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (distanceInFeet > 2500) {
+    return 'No can do.';
+  } else {
+    return 'That is too far for me.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination) {
+  return destination === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount) {
+  switch (tipAmount) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
